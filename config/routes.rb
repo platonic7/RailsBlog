1750d89ba29046029afb3bb0b380a8d4a0b2580c
Platonic <platonic1215@gmail.com>
2017-02-05 23:44:03 +0900

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :mycontacts
+  root 'mycontacts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
